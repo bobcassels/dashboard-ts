decb90d7c623c38835b58d0736f9c32f6fe66576
Bob Cassels <bobcassels@netscape.net>
2023-06-29 11:48:44 -0400

    Mostly have new router working

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-Meteor.startup(() => {
-  const container = document.getElementById('react-target');
-  const root = createRoot(container!);
-  root.render(<App />);
-});
+import App from "../imports/ui/App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,25 +1,119 @@
-import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import CssBaseline from '@mui/material/CssBaseline';
-import { Hello } from './Hello';
-import { Info } from './Info';
 
-export const App = () => (
-  <div>
-    <Helmet>
-      <title>{Meteor.settings.public.title}</title>
-      <link rel="icon" type="image/x-icon" href={Meteor.settings.public.favicon}/>
-      <link rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    </Helmet>
-    <CssBaseline />
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-  </div>
-);
+export default function App() {
+    const title = Meteor.settings.public.title;
+    return (
+        <div>
+            <Helmet>
+                <title>{title}</title>
+                <link rel="icon" type="image/x-icon" href={Meteor.settings.public.favicon} />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link rel="stylesheet"
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                />
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Helmet>
+            <CssBaseline />
+
+            <h1>{title}</h1>
+
+            <p>
+                This example demonstrates some of the core features of React Router
+                including nested <code>&lt;Route&gt;</code>s,{" "}
+                <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
+                "*" route (aka "splat route") to render a "not found" page when someone
+                visits an unrecognized URL.
+            </p>
+
+            {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+
+                    {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </div>
+    );
+}
+
+function Layout() {
+    return (
+        <div>
+            {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/nothing-here">Nothing Here</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <hr />
+
+            {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+            <Outlet />
+        </div>
+    );
+}
+
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
+}
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
+
+function NotFound() {
+    return (
+        <div>
+            <h2>404 Not Found</h2>
+            <p>
+                <Link to="/">Go to the home page</Link>
+            </p>
+        </div>
+    );
+}
